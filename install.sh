924f2c6d98cca9374e4dcf7372dd4acec59e1d61
jukoo <funscript@outlook.fr>
2019-06-08 12:43:35 +0000

    avoiding redundant tui installer

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ run_dialog() {
 
 install_dialog() {
   if [ ! "$(which dialog 2> /dev/null)" ]; then
-    prompt -i "\n 'dialog' needs to be installed for this shell "
+    prompt -i "\n 'dialog' needs to be installed for this shell"
     if has_command zypper; then
 
       sudo zypper in dialog
@@ -197,13 +197,12 @@ if [[ $# -lt 1 ]] && [[ $UID -ne $ROOT_UID ]]; then
   # persisted execution of the script as root
   read -p "[ trusted ] specify the root password : " -t${MAX_DELAY} -s
   [[ -n "$REPLY" ]]&& {
-    sudo -S <<< $REPLY $0
+   exec sudo -S <<< $REPLY $0
   }|| {
     prompt  "\n Operation canceled  Bye"
     exit 1
   }
 
-  run_dialog
 fi
 
 while [[ $# -ge 1 ]]; do
@@ -234,5 +233,6 @@ while [[ $# -ge 1 ]]; do
 done
 
 install_dialog && install
+test $? -eq 0 && prompt  -w "* At the next restart of your computer you can admire your new Grub theme named << ${theme} >>" 
 
 exit 0
