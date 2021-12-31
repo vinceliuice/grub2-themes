da7b6426401587fc3314ee3ac6a6a42208f68920
Erik Dubois <erik.dubois@gmail.com>
2021-12-31 15:15:34 +0100

    compare with dev

---
+++ setup-git-v5.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+project=$(basename `pwd`)
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/erikdubois/"$project
+echo "-----------------------------------------------------------------------------"
+git config --global pull.rebase false
+git config --global user.name "Erik Dubois"
+git config --global user.email "erik.dubois@gmail.com"
+sudo git config --system core.editor nano
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
+git config --global push.default simple
+
+git remote set-url origin git@github.com-edu:erikdubois/$project
+
+echo "Everything set"
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
